fc85c74788fb55c6e2d25cc395f05e2da772c50f
1MuhireDavid <muhiredavid74@gmail.com>
2026-03-15 12:18:38 +0200

    add static root files

--- ikigembe_bn/settings.py
+++ ikigembe_bn/settings.py
@@ -196,10 +196,9 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Determine storage based on USE_S3 setting (not DEBUG)
 USE_S3 = True 
